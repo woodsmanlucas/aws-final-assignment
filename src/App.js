466e1b3d5e272b43e2a0489f50957a000cbe4b39
Lucas Johnson <A01199573@ad.bcit.ca>
2020-01-20 15:02:37 -0800

    added logout button

--- src/App.js
+++ src/App.js
@@ -1,57 +1,73 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Register from './components/auth/Register';
-import Welcome from './components/auth/Welcome';
-import LogIn from './components/auth/LogIn';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Register from "./components/auth/Register";
+import Welcome from "./components/auth/Welcome";
+import LogIn from "./components/auth/LogIn";
+import { Auth } from "aws-amplify";
+import { getElementError } from "@testing-library/react";
 
 class App extends Component {
-    state = {
-      isAuth: false,
-      user: null
-    }
-
+  state = {
+    isAuth: false,
+    user: null
+  };
 
   authenticateUser = authenticated => {
-    this.setState({isAuth: authenticated})
-  }
+    this.setState({ isAuth: authenticated });
+  };
 
-  setAuthUser = user  => {
-    this.setState({user: user})
+  setAuthUser = user => {
+    console.log(user);
+    this.setState({ user: user });
+  };
+
+  async componentDidMount() {
+    try {
+      const session = await Auth.currentSession();
+      this.authenticateUser(true);
+      console.log(session);
+      const user = await Auth.currentAuthenticatedUser();
+      this.setAuthUser(user);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   render() {
     const authProps = {
       isAuth: this.state.isAuth,
-      user: this.state.authenticateUser,
+      user: this.state.user,
       authenticateUser: this.authenticateUser,
       setAuthUser: this.setAuthUser
-    }
+    };
 
     return (
-      <div className="App">
-        <Router>
-          <div>
-            <Navbar auth={authProps} />
-            <Switch>
-              <Route exact path="/">
-                <Home auth={authProps} />
-              </Route>
-              <Route exact path="/register" >
-                <Register auth={authProps} />
-              </Route>
-              <Route exact path="/welcome" component={Welcome} >
-                <Welcome auth={authProps} />
-              </Route>
-              <Route exact path="/login" component={LogIn} >
-                <LogIn auth={authProps} />
-              </Route>
-            </Switch>
-          </div>
-        </Router>
-      </div>
+      !this.state.checkingAuth && (
+        <div className="App">
+          <Router>
+            <div>
+              <Navbar auth={authProps} />
+              <Switch>
+                <Route exact path="/">
+                  <Home auth={authProps} />
+                </Route>
+                <Route exact path="/register">
+                  <Register auth={authProps} />
+                </Route>
+                <Route exact path="/welcome">
+                  <Welcome auth={authProps} />
+                </Route>
+                <Route exact path="/login">
+                  <LogIn auth={authProps} />
+                </Route>
+              </Switch>
+            </div>
+          </Router>
+        </div>
+      )
     );
   }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Navbar extends Component {
+  logOutHandler = async e => {
+    e.preventDefault();
+    try {
+      this.props.auth.authenticateUser(false);
+      this.props.auth.setAuthUser(null);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   render() {
     return (
       <nav className="navbar">
@@ -12,18 +22,33 @@ export default class Navbar extends Component {
           </div>
           <div className="navbar-end">
             <div className="navbar-item">
+              {console.log(this.props.auth.user)}
+              {this.props.auth.isAuth && this.props.auth.user && (
+                <p>Hello {this.props.auth.user.username}</p>
+              )}
               <div className="auth-buttons">
-                <a href="/register" className="button is-primary">
-                  <strong>Register</strong>
-                </a>
-                <a href="/login" className="button is-light">
-                  Log in
-                </a>
+                {this.props.auth.isAuth ? (
+                  <button
+                    onClick={this.logOutHandler.bind(this)}
+                    className="button is-light"
+                  >
+                    Log out
+                  </button>
+                ) : (
+                  <>
+                    <a href="/register" className="button is-primary">
+                      <strong>Register</strong>
+                    </a>
+                    <a href="/login" className="button is-light">
+                      Log in
+                    </a>
+                  </>
+                )}
               </div>
             </div>
           </div>
         </div>
       </nav>
-    )
+    );
   }
 }
